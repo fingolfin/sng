88e36b693bd1415dce1a148da2e16e3228141e62
Eric S. Raymond <esr@thyrsus.com>
2010-11-22 12:56:23 -0500

    Add autogen.sh for easier bootstrapping.

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script runs commands necessary to generate a Makefile for sng.
+
+echo "Warning: This script will run configure for you -- if you need to pass"
+echo "  arguments to configure, please give them as arguments to this script."
+
+aclocal
+autoheader
+automake --add-missing
+autoconf
+automake
+
+configure $*
+
+exit 0
